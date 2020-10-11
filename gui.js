eba584045792088cd999efc4d7e4c57f1610409e
Santiago Acevedo <47859647+SantiAce26@users.noreply.github.com>
2020-10-11 00:37:43 -0400

    Update gui.js

--- gui.js
+++ gui.js
@@ -1,17 +1,17 @@
 (function () {
     var files = [
-        "songsGenerated/generated/2020-10-10_180813_01.mp3",
-        "songsGenerated/generated/2020-10-10_174934_01.mp3",
-        "songsGenerated/generated/2020-10-10_175358_01.mp3",
-        "songsGenerated/generated/2020-10-10_175358_02.mp3",
-        "songsGenerated/generated/2020-10-10_180813_01.mp3",
-        "songsGenerated/generated/2020-10-10_180813_02.mp3",
-        "songsGenerated/generated/2020-10-10_201413_1.mp3",
-        "songsGenerated/generated/2020-10-10_201413_2.mp3",
-        "songsGenerated/generated/2020-10-10_201616_1.mp3",
-        "songsGenerated/generated/2020-10-10_201616_2.mp3",
-        "songsGenerated/generated/2020-10-10_223234_1.mp3",
-        "songsGenerated/generated/2020-10-10_223234_2.mp3"
+        "songsGenerated/2020-10-10_180813_01.mp3",
+        "songsGenerated/2020-10-10_174934_01.mp3",
+        "songsGenerated/2020-10-10_175358_01.mp3",
+        "songsGenerated/2020-10-10_175358_02.mp3",
+        "songsGenerated/2020-10-10_180813_01.mp3",
+        "songsGenerated/2020-10-10_180813_02.mp3",
+        "songsGenerated/2020-10-10_201413_1.mp3",
+        "songsGenerated/2020-10-10_201413_2.mp3",
+        "songsGenerated/2020-10-10_201616_1.mp3",
+        "songsGenerated/2020-10-10_201616_2.mp3",
+        "songsGenerated/2020-10-10_223234_1.mp3",
+        "songsGenerated/2020-10-10_223234_2.mp3"
     ];
     var i = 0;
 
