bb302306c2aa815367be709c416fa4b8ad3af112
santiago Acevedo <santiaacevedo26@gmail.com>
2020-10-11 00:52:05 -0400

    final removal

--- generated/textGetter.py
+++
@@ -1,8 +0,0 @@
-import os
-from os  import listdir
-from os.path import isfile,join
-#mypath = "C:\Users\santi\Documents\musicGenerator\musicOutput"
-onlyfiles = os.listdir('.')
-with open('file_text','w') as f:
-	for item in onlyfiles:
-		f.write("%s\n" % item)
